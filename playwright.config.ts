8163f2eeccdb2e326bbbf061861d0fd1bdd53b4f
anu-01 <anurao.1991@gmail.com>
2024-01-10 18:08:12 +0000

    update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,8 +10,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: 'api/tests/',
-  testMatch: 'api/tests/*.spec.ts',
+  testDir: 'web/tests',
+  // testMatch: '*/tests/*.spec.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
