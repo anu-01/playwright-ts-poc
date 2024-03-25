e25df96ec59011c91044e21cb5f7f0c43ae03e34
anu-01 <anurao.1991@gmail.com>
2024-03-25 12:30:46 +0000

    added more tests

--- web/tests/cartTest.spec.ts
+++ web/tests/cartTest.spec.ts
@@ -4,7 +4,7 @@ import { StandardUser, VisualUser } from '../testdata/creds.ts';
 import LoginPage from "../pages/loginPage.ts";
 import CartPage from "../pages/cartPage.ts";
 
-test.describe('Cart Functionality for standard user', () => {
+test.describe('Cart Functionality 1 for standard user', () => {
     let loginPage: LoginPage
     let cartPage: CartPage
 
@@ -33,6 +33,210 @@ test.describe('Cart Functionality for standard user', () => {
         await cartPage.validateProductsInCartisNot('Sauce Labs Backpack');
     });
     test('Continue shopping', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.continueShopping();
+        await expect(page.url()).toContain('inventory.html');
+    });
+
+    test.afterEach(async ({ page }) => { 
+        // page.close();      
+    })
+});
+
+
+test.describe('Cart Functionality 2 for standard user', () => {
+    let loginPage: LoginPage
+    let cartPage: CartPage
+
+    test.use({ storageState: 'playwright/.auth/standarduser.json' });
+
+    test.beforeEach(async ({ page }) => {       
+        cartPage = new CartPage(page);
+        loginPage = new LoginPage(page);
+        await loginPage.open();
+        await loginPage.login(StandardUser.username, StandardUser.password);
+        await loginPage.verifyLogin();
+    })
+
+    test('Add product to cart and checkout 2', async ({ page }) => { 
+        // await page.goto('/');       
+        await page.getByAltText('Products').isVisible();
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.validateProductsInCart('Sauce Labs Backpack');
+        await cartPage.checkout();
+    });
+    test('Remove product from cart 2', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.removeProductFromCart('Sauce Labs Backpack');
+        await cartPage.validateProductsInCartisNot('Sauce Labs Backpack');
+    });
+    test('Continue shopping 2', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.continueShopping();
+        await expect(page.url()).toContain('inventory.html');
+    });
+
+    test.afterEach(async ({ page }) => { 
+        // page.close();      
+    })
+});
+
+
+test.describe('Cart Functionality 3 for standard user', () => {
+    let loginPage: LoginPage
+    let cartPage: CartPage
+
+    test.use({ storageState: 'playwright/.auth/standarduser.json' });
+
+    test.beforeEach(async ({ page }) => {       
+        cartPage = new CartPage(page);
+        loginPage = new LoginPage(page);
+        await loginPage.open();
+        await loginPage.login(StandardUser.username, StandardUser.password);
+        await loginPage.verifyLogin();
+    })
+
+    test('Add product to cart and checkout 3', async ({ page }) => { 
+        // await page.goto('/');       
+        await page.getByAltText('Products').isVisible();
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.validateProductsInCart('Sauce Labs Backpack');
+        await cartPage.checkout();
+    });
+    test('Remove product from cart 3', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.removeProductFromCart('Sauce Labs Backpack');
+        await cartPage.validateProductsInCartisNot('Sauce Labs Backpack');
+    });
+    test('Continue shopping 3', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.continueShopping();
+        await expect(page.url()).toContain('inventory.html');
+    });
+
+    test.afterEach(async ({ page }) => { 
+        // page.close();      
+    })
+});
+
+
+test.describe('Cart Functionality 4 for standard user', () => {
+    let loginPage: LoginPage
+    let cartPage: CartPage
+
+    test.use({ storageState: 'playwright/.auth/standarduser.json' });
+
+    test.beforeEach(async ({ page }) => {       
+        cartPage = new CartPage(page);
+        loginPage = new LoginPage(page);
+        await loginPage.open();
+        await loginPage.login(StandardUser.username, StandardUser.password);
+        await loginPage.verifyLogin();
+    })
+
+    test('Add product to cart and checkout 4', async ({ page }) => { 
+        // await page.goto('/');       
+        await page.getByAltText('Products').isVisible();
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.validateProductsInCart('Sauce Labs Backpack');
+        await cartPage.checkout();
+    });
+    test('Remove product from cart 4', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.removeProductFromCart('Sauce Labs Backpack');
+        await cartPage.validateProductsInCartisNot('Sauce Labs Backpack');
+    });
+    test('Continue shopping 4', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.continueShopping();
+        await expect(page.url()).toContain('inventory.html');
+    });
+
+    test.afterEach(async ({ page }) => { 
+        // page.close();      
+    })
+});
+
+
+test.describe('Cart Functionality 5 for standard user', () => {
+    let loginPage: LoginPage
+    let cartPage: CartPage
+
+    test.use({ storageState: 'playwright/.auth/standarduser.json' });
+
+    test.beforeEach(async ({ page }) => {       
+        cartPage = new CartPage(page);
+        loginPage = new LoginPage(page);
+        await loginPage.open();
+        await loginPage.login(StandardUser.username, StandardUser.password);
+        await loginPage.verifyLogin();
+    })
+
+    test('Add product to cart and checkout 5', async ({ page }) => { 
+        // await page.goto('/');       
+        await page.getByAltText('Products').isVisible();
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.validateProductsInCart('Sauce Labs Backpack');
+        await cartPage.checkout();
+    });
+    test('Remove product from cart 5', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.removeProductFromCart('Sauce Labs Backpack');
+        await cartPage.validateProductsInCartisNot('Sauce Labs Backpack');
+    });
+    test('Continue shopping 5', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.continueShopping();
+        await expect(page.url()).toContain('inventory.html');
+    });
+
+    test.afterEach(async ({ page }) => { 
+        // page.close();      
+    })
+});
+
+test.describe('Cart Functionality 6 for standard user', () => {
+    let loginPage: LoginPage
+    let cartPage: CartPage
+
+    test.use({ storageState: 'playwright/.auth/standarduser.json' });
+
+    test.beforeEach(async ({ page }) => {       
+        cartPage = new CartPage(page);
+        loginPage = new LoginPage(page);
+        await loginPage.open();
+        await loginPage.login(StandardUser.username, StandardUser.password);
+        await loginPage.verifyLogin();
+    })
+
+    test('Add product to cart and checkout 6', async ({ page }) => { 
+        // await page.goto('/');       
+        await page.getByAltText('Products').isVisible();
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.validateProductsInCart('Sauce Labs Backpack');
+        await cartPage.checkout();
+    });
+    test('Remove product from cart 6', async ({ page }) => {
+        await cartPage.addProductToCart('Sauce Labs Backpack');
+        await cartPage.clickCart();
+        await cartPage.removeProductFromCart('Sauce Labs Backpack');
+        await cartPage.validateProductsInCartisNot('Sauce Labs Backpack');
+    });
+    test('Continue shopping 6', async ({ page }) => {
         await cartPage.addProductToCart('Sauce Labs Backpack');
         await cartPage.clickCart();
         await cartPage.continueShopping();
