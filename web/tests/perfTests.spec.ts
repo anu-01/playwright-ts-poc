ce60e9b0d8a8b0bade3b04a7c8b570364b1cc692
Anusha Ammaluru <anammalu@microsoft.com>
2025-04-11 12:30:54 +0100

    PerfTest added for latency measures

--- web/tests/perfTests.spec.ts
+++ web/tests/perfTests.spec.ts
@@ -1,25 +1,35 @@
-import { test, expect, TestInfo, Page } from '@playwright/test';
+import { test, expect, Page, TestInfo } from '@playwright/test';
 
-
-  test('performance test example', async ({ page }, TestInfo) => {
-    // Perform actions
-    await page.goto('https://www.saucedemo.com/');
-    // Measure performance
-    await measurePerformance(page, TestInfo);
+test('Performance test example', async ({ page }, testInfo) => {
+  await page.goto('https://www.saucedemo.com/');
+  await measurePerformance(page, testInfo);
 });
 
-
-async function measurePerformance(page: Page, TestInfo: TestInfo) {
-  // Use Performance API to measure performance 
-  // https://developer.mozilla.org/en-US/docs/Web/API/Performance/getEntriesByType
-  const [performanceTiming] = await page.evaluate(() => {
-      const [timing] = performance.getEntriesByType('navigation');
-      return [timing];
+async function measurePerformance(page: Page, testInfo: TestInfo) {
+  const performanceTiming = await page.evaluate(() => {
+    return performance.getEntriesByType('navigation')[0] as PerformanceNavigationTiming;;
   });
 
-  console.log(performanceTiming);
-  // Get the start to load event end time
-  const startToLoadEventEnd = performanceTiming.loadEventEnd - performanceTiming.startTime;
-  // Add the performance annotation to the HTML report
-  test.info().annotations.push({ type: 'Performance', description: `"${TestInfo.project.name}" - Navigation start to load event end: ${startToLoadEventEnd}ms` });
+  const {
+    startTime,
+    responseStart,
+    domInteractive,
+    domContentLoadedEventEnd,
+    loadEventEnd,
+  } = performanceTiming;
+
+  const ttfb = responseStart - startTime;
+  const domInteractiveTime = domInteractive - startTime;
+  const dcl = domContentLoadedEventEnd - startTime;
+  const loadTime = loadEventEnd - startTime;
+
+  console.log(`TTFB: ${ttfb.toFixed(2)}ms`);
+  console.log(`DOM Interactive: ${domInteractiveTime.toFixed(2)}ms`);
+  console.log(`DCL: ${dcl.toFixed(2)}ms`);
+  console.log(`Load Event End: ${loadTime.toFixed(2)}ms`);
+
+  testInfo.annotations.push({
+    type: 'Performance',
+    description: `TTFB: ${ttfb.toFixed(2)}ms, LoadTime: ${loadTime.toFixed(2)}ms`,
+  });
 }
